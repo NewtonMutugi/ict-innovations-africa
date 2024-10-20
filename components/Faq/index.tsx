fdd874c40f90f78eccf2cd7aeff44753797d6935
NewtonMutugi <newtonmut197@gmail.com>
2024-10-20 19:34:25 +0300

    Updated FAQ component to use dynamic questions data and enhance layout with section title

--- components/Faq/index.tsx
+++ components/Faq/index.tsx
@@ -4,48 +4,40 @@ import {
   AccordionTitle,
   AccordionContent,
 } from "flowbite-react";
+import SectionTitle from "../Common/SectionTitle";
+import questionsData from "./questionsData";
 
 const Faq = () => {
   return (
     <section id="faq">
-      <div className="border-b border-body-color/[.15] py-8 pb-16 dark:border-white/[.15] md:py-16">
+      <div className="border-b border-body-color/[.15] py-16 pb-16 dark:border-white/[.15] md:py-16">
+        <SectionTitle
+          title="Frequently Asked Questions"
+          paragraph=""
+          center
+          mb="20px"
+        />
         <div className="container mx-auto px-4">
-          <div className="grid grid-cols-1 gap-8 lg:grid-cols-2">
-            <div>
-              <p className="mb-2 text-sm font-bold uppercase text-primary">
-                faq
-              </p>
+          <div className="grid grid-cols-1 gap-8 pt-10 lg:grid-cols-2">
+            <div className="flex flex-col justify-center">
               <h2 className="text-secondary mb-4 text-3xl font-semibold">
                 Foremost Common <span className="text-primary">Questions</span>
               </h2>
               <p className="text-gray-600">
-                Curious about how Essay Sprint ensures authenticity or its
+                Curious about how WebGenerator.ai ensures authenticity or its
                 usability? Explore these commonly asked questions for insights
                 into our innovative platform.
               </p>
+              <p className="text-gray-600">
+                Have more questions? Feel free to reach out to our support team
+                for assistance.
+              </p>
             </div>
             <div>
-              <Accordion alwaysOpen>
-                <AccordionItem
-                  title="How does Essay Sprint ensure that the generated essays are not detected as AI-generated or plagiarized?"
-                  body="Essay Sprint utilizes sophisticated algorithms to mimic the writing style and voice of the user, ensuring that each essay is unique and original. Additionally, our tool cross-references content with extensive databases to avoid plagiarism."
-                />
-                <AccordionItem
-                  title="Can I trust Essay Sprint with sensitive information and data?"
-                  body="Absolutely. We prioritize the privacy and security of our users' data. Essay Sprint adheres to stringent data protection measures, ensuring that all information remains confidential and secure."
-                />
-                <AccordionItem
-                  title="Is Essay Sprint suitable for all academic levels and subjects?"
-                  body="Yes, Essay Sprint is designed to accommodate a wide range of academic disciplines and levels. Whether you're a high school student or a doctoral candidate, our tool can assist you in generating compelling essays across various subjects."
-                />
-                <AccordionItem
-                  title="How user-friendly is the Essay Sprint interface?"
-                  body="Essay Sprint boasts an intuitive and user-friendly interface, making the essay generation process smooth and straightforward. Even users with minimal technical expertise can navigate the platform with ease."
-                />
-                <AccordionItem
-                  title="Does Essay Sprint offer any support or assistance during the essay writing process?"
-                  body="Absolutely. Our dedicated support team is available to assist you every step of the way. Whether you have questions about using the tool or need guidance on refining your essay, we're here to help you achieve academic excellence."
-                />
+              <Accordion>
+                {questionsData.map(({ question, answer }, index) => (
+                  <AccordionItem key={index} title={question} body={answer} />
+                ))}
               </Accordion>
             </div>
           </div>
---
+++ components/Faq/questionsData.tsx
@@ -0,0 +1,48 @@
+import { Menu } from "@/types/menu";
+
+const questionsData = [
+  {
+    id: 1,
+    question: "What Languages Are Supported?",
+    answer:
+      "You can create websites in the following languages: English, Japanese, Korean, Arabic, Bahasa Indonesia, Bengali, Bulgarian, Chinese (Simplified), Chinese (Traditional), Croatian, Czech, Danish, Dutch, Estonian, Farsi, Finnish, 'French', 'German', 'Gujarati', 'Greek', Hebrew, Hindi, Hungarian, Italian, Kannada, Latvian, 'Lithuanian', Malayalam, Marathi, Norwegian, Polish, Portuguese, Romanian, Russian, Serbian, Slovak, Slovenian, Spanish, Swahili, Swedish, Tamil, Telugu, Thai, Turkish, Ukrainian, Urdu, Vietnamese.",
+  },
+  {
+    id: 2,
+    question: "Is there A Money Back Guarantee?",
+    answer:
+      "Yes, 30 day money back guarantee is included if you are not satisfied. ",
+  },
+  {
+    id: 3,
+    question: "Who Hosts The Website?",
+    answer:
+      "You can download all the websites you build with the app as HTML and host them anywhere for full flexibility. You automate website creation A to Z with AI or create from scratch. WE DO NOT HOST THE WEBSITES.",
+  },
+  {
+    id: 4,
+    question: "What Happens Once I Create 50 Websites?",
+    answer:
+      "Since we offer our app with premium AI capabilities at a one-time price, you can purchase more accounts once you create 50 websites at the same price shown on this website.",
+  },
+  {
+    id: 5,
+    question: "Are there any annual fee?",
+    answer:
+      "We do not charge any annual fee. The payment is a one-off payment.",
+  },
+  {
+    id: 6,
+    question: "Is Step-by-Step Training Included?",
+    answer:
+      "YES - This comes with a step-by-step video training walking you through the entire process of creating a website.",
+  },
+  {
+    id: 7,
+    question: "Can I Sell The Websites I Create?",
+    answer:
+      "Yes, you can host and sell the websites you create. You can download the websites you create and send them to your clients or host the sites on their own hosting service.",
+  },
+];
+
+export default questionsData;
