c317bec15b4bc2cf0c5b3165cd89504fb49540a2
NewtonMutugi <newtonmut197@gmail.com>
2024-10-21 10:54:27 +0300

    Update Hero component link to direct users to signup page

--- components/Hero/index.tsx
+++ components/Hero/index.tsx
@@ -33,7 +33,7 @@ const Hero = () => {
                     🔥 Get Pro
                   </Link> */}
                   <Link
-                    href="https://github.com/NextJSTemplates/startup-nextjs"
+                    href="/signup"
                     className="inline-block rounded-sm bg-black px-8 py-4 text-base font-semibold text-white duration-300 ease-in-out hover:bg-black/90 dark:bg-white/10 dark:text-white dark:hover:bg-white/5"
                   >
                     Get AI Website Builder Here
