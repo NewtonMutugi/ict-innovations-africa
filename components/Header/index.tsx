7c4fb90a29151d728bdfd988a2884f39dc08a98e
NewtonMutugi <newtonmut197@gmail.com>
2024-10-24 14:24:24 +0300

    Comment out login and register links in Header component

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -166,7 +166,7 @@ const Header = () => {
                 </nav>
               </div>
               <div className="flex items-center justify-end pr-16 lg:pr-0">
-                <Link
+                {/* <Link
                   href="https://app.webgenerator.ai/auth/login"
                   className="hidden px-7 py-3 text-base font-medium text-dark hover:opacity-70 dark:text-white md:block"
                 >
@@ -177,7 +177,7 @@ const Header = () => {
                   className="ease-in-up no-wrap hidden rounded-sm bg-primary px-8 py-3 text-base font-medium text-white shadow-btn transition duration-300 hover:bg-opacity-90 hover:shadow-btn-hover md:block md:px-9 lg:px-6 xl:px-9"
                 >
                   Register
-                </Link>
+                </Link> */}
                 <div>
                   <ThemeToggler />
                 </div>
