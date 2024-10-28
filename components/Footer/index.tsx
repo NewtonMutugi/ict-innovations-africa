778b28fb05b8057d8b31646b56b35498017abea4
NewtonMutugi <newtonmut197@gmail.com>
2024-10-28 19:42:30 +0300

    Update footer links for improved navigation and clarity

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -116,25 +116,25 @@ const Footer = () => {
                   </li>
                   <li>
                     <a
-                      href="/#pricing"
+                      href="/#services"
                       className="mb-4 inline-block text-base text-body-color duration-300 hover:text-primary dark:text-body-color-dark dark:hover:text-primary"
                     >
-                      Pricing
+                      About us
                     </a>
                   </li>
                   <li>
                     <a
-                      href="/#features"
+                      href="/#"
                       className="mb-4 inline-block text-base text-body-color duration-300 hover:text-primary dark:text-body-color-dark dark:hover:text-primary"
                     >
-                      Features
+                      Products
                     </a>
                   </li>
                 </ul>
               </div>
             </div>
 
-            <div className="w-full px-4 sm:w-1/2 md:w-1/2 lg:w-2/12 xl:w-2/12">
+            {/* <div className="w-full px-4 sm:w-1/2 md:w-1/2 lg:w-2/12 xl:w-2/12">
               <div className="mb-12 lg:mb-16">
                 <h2 className="mb-10 text-xl font-bold text-black dark:text-white">
                   Terms
@@ -166,7 +166,7 @@ const Footer = () => {
                   </li>
                 </ul>
               </div>
-            </div>
+            </div> */}
 
             <div className="w-full px-4 md:w-1/2 lg:w-4/12 xl:w-3/12">
               <div className="mb-12 lg:mb-16">
@@ -192,10 +192,10 @@ const Footer = () => {
                   </li>
                   <li>
                     <a
-                      href="/"
+                      href="/#about"
                       className="mb-4 inline-block text-base text-body-color duration-300 hover:text-primary dark:text-body-color-dark dark:hover:text-primary"
                     >
-                      About
+                      About us
                     </a>
                   </li>
                 </ul>
