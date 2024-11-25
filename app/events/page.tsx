98ac3c40cc01bb34592d35d2eaf8b00f5771ac28
NewtonMutugi <newtonmut197@gmail.com>
2024-11-25 13:40:43 +0300

    Fix metadata title formatting and add favicon identifier

--- app/events/page.tsx
+++ app/events/page.tsx
@@ -5,7 +5,7 @@ import { Metadata } from "next";
 import SectionTitle from "@/components/Common/SectionTitle";
 
 export const metadata: Metadata = {
-  title: "Events| ICT Innnovations Africa",
+  title: "Events | ICT Innnovations Africa",
   description:
     "This is the event page of ICT Innovations Africa. Here you can find all the latest events and engagements that we are involved in.",
   // other metadata
