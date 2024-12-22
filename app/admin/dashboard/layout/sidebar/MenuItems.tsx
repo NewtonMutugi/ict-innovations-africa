d748be63a9e4e62695b9a0a69ce134c4ca120780
NewtonMutugi <newtonmut197@gmail.com>
2024-12-23 02:18:08 +0300

    Remove commented-out utility and auth menu items from sidebar

--- app/admin/dashboard/layout/sidebar/MenuItems.tsx
+++ app/admin/dashboard/layout/sidebar/MenuItems.tsx
@@ -39,54 +39,54 @@ const Menuitems = [
     href: "/admin/dashboard/events/add",
   },
 
-  {
-    navlabel: true,
-    subheader: "Utilities",
-  },
-  {
-    id: uniqueId(),
-    title: "Typography",
-    icon: IconTypography,
-    href: "/admin/dashboard/utilities/typography",
-  },
-  {
-    id: uniqueId(),
-    title: "Shadow",
-    icon: IconCopy,
-    href: "/admin/dashboard/utilities/shadow",
-  },
-  {
-    navlabel: true,
-    subheader: "Auth",
-  },
-  {
-    id: uniqueId(),
-    title: "Login",
-    icon: IconLogin,
-    href: "/admin/dashboard/signin",
-  },
-  {
-    id: uniqueId(),
-    title: "Register",
-    icon: IconUserPlus,
-    href: "/admin/dashboard/signup",
-  },
-  {
-    navlabel: true,
-    subheader: "Extra",
-  },
-  {
-    id: uniqueId(),
-    title: "Icons",
-    icon: IconMoodHappy,
-    href: "/admin/dashboard/icons",
-  },
-  {
-    id: uniqueId(),
-    title: "Sample Page",
-    icon: IconAperture,
-    href: "/admin/dashboard/sample-page",
-  },
+  // {
+  //   navlabel: true,
+  //   subheader: "Utilities",
+  // },
+  // {
+  //   id: uniqueId(),
+  //   title: "Typography",
+  //   icon: IconTypography,
+  //   href: "/admin/dashboard/utilities/typography",
+  // },
+  // {
+  //   id: uniqueId(),
+  //   title: "Shadow",
+  //   icon: IconCopy,
+  //   href: "/admin/dashboard/utilities/shadow",
+  // },
+  // {
+  //   navlabel: true,
+  //   subheader: "Auth",
+  // },
+  // {
+  //   id: uniqueId(),
+  //   title: "Login",
+  //   icon: IconLogin,
+  //   href: "/admin/dashboard/signin",
+  // },
+  // {
+  //   id: uniqueId(),
+  //   title: "Register",
+  //   icon: IconUserPlus,
+  //   href: "/admin/dashboard/signup",
+  // },
+  // {
+  //   navlabel: true,
+  //   subheader: "Extra",
+  // },
+  // {
+  //   id: uniqueId(),
+  //   title: "Icons",
+  //   icon: IconMoodHappy,
+  //   href: "/admin/dashboard/icons",
+  // },
+  // {
+  //   id: uniqueId(),
+  //   title: "Sample Page",
+  //   icon: IconAperture,
+  //   href: "/admin/dashboard/sample-page",
+  // },
 ];
 
 export default Menuitems;
