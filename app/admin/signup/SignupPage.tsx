a84013637334f546c5b9c933bc1900078b4e9a43
NewtonMutugi <newtonmut197@gmail.com>
2024-12-24 03:31:54 +0300

    Update placeholder text in SignupPage input field to reflect username entry

--- app/admin/signup/SignupPage.tsx
+++ app/admin/signup/SignupPage.tsx
@@ -145,7 +145,7 @@ const SignupPage = () => {
                     <input
                       type="text"
                       name="username"
-                      placeholder="Enter your preferred email"
+                      placeholder="Enter your preferred username"
                       className="w-full rounded-sm border border-stroke bg-[#f8f8f8] px-6 py-3 text-base text-body-color outline-none transition-all duration-300 focus:border-primary dark:border-transparent dark:bg-[#2C303B] dark:text-body-color-dark dark:shadow-two dark:focus:border-primary dark:focus:shadow-none"
                       required={true}
                     />
