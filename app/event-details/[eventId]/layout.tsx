dfed3f6a4204718b6d2aa35df3262d5a58666455
NewtonMutugi <newtonmut197@gmail.com>
2024-12-29 21:17:09 +0300

    Disabled generate event metadata

--- app/event-details/[eventId]/layout.tsx
+++ app/event-details/[eventId]/layout.tsx
@@ -1,34 +1,34 @@
-import { Metadata } from "next";
-import eventData from "@/components/Events/eventData";
+// import { Metadata } from "next";
+// import eventData from "@/components/Events/eventData";
 
-export async function generateMetadata({
-  params,
-}: {
-  params: { eventId: string };
-}): Promise<Metadata> {
-  const { eventId } = params;
+// export async function generateMetadata({
+//   params,
+// }: {
+//   params: { eventId: string };
+// }): Promise<Metadata> {
+//   const { eventId } = params;
 
-  // Fetch metadata for the event (replace with your API or logic)
-  const event = await fetchEventDetails(eventId);
+//   // Fetch metadata for the event (replace with your API or logic)
+//   // const event = await fetchEventDetails(eventId);
 
-  if (!event) {
-    return {
-      title: "Event Not Found",
-      description: "No details available for this event.",
-    };
-  }
+//   if (!event) {
+//     return {
+//       title: "Event Not Found",
+//       description: "No details available for this event.",
+//     };
+//   }
 
-  return {
-    title: `${event.title} | ICT Innovations Africa`,
-    description: event.description,
-  };
-}
+//   return {
+//     title: `${event.title} | ICT Innovations Africa`,
+//     description: event.description,
+//   };
+// }
 
-async function fetchEventDetails(eventId: string) {
-  // Simulate API call or database query
-  const events = eventData;
-  return events.find((event) => event.id === Number(eventId));
-}
+// async function fetchEventDetails(eventId: string) {
+//   // Simulate API call or database query
+//   const events = eventData;
+//   return events.find((event) => event.id === Number(eventId));
+// }
 
 export default function EventDetailsLayout({
   children,
