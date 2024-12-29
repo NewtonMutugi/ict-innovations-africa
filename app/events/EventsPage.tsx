c51fe75d528861e9d187ecfdb177ea720d6bc9bb
NewtonMutugi <newtonmut197@gmail.com>
2024-12-29 19:31:55 +0300

    Moved Events List page to separate component

---
+++ app/events/EventsPage.tsx
@@ -0,0 +1,38 @@
+import SectionTitle from '@/components/Common/SectionTitle';
+import eventData from '@/components/Events/eventData';
+import SingleEvent from '@/components/Events/SingleEvent';
+import React from 'react'
+
+const EventsPage = () => {
+  return (
+    <>
+      {/* <Breadcrumb
+        pageName="Events"
+        description="Here are some of our latest upcoming events and engagements. This would be a perfect opportunity to learn more about what we do and what we are all about."
+      /> */}
+      <section className="pb-[120px] pt-[120px]">
+        <SectionTitle
+          title="Our Latest Events"
+          paragraph="Here are some of our latest upcoming events and engagements. This would be a perfect opportunity to learn more about what we do and what we are all about."
+          center
+          buttonText="Register Interest"
+          buttonTextLink="https://forms.gle/gkxDxfg6RWreZBncA"
+        />
+        <div className="container">
+          <div className="-mx-4 flex flex-wrap justify-center">
+            {eventData.map((event) => (
+              <div
+                key={event.id}
+                className="w-full px-4 md:w-2/3 lg:w-1/2 xl:w-1/3"
+              >
+                <SingleEvent event={event} />
+              </div>
+            ))}
+          </div>
+        </div>
+      </section>
+    </>
+  );
+}
+
+export default EventsPage
--- app/events/page.tsx
+++ app/events/page.tsx
@@ -1,8 +1,5 @@
-import SingleEvent from "@/components/Events/SingleEvent";
-import eventData from "@/components/Events/eventData";
-import Breadcrumb from "@/components/Common/Breadcrumb";
 import { Metadata } from "next";
-import SectionTitle from "@/components/Common/SectionTitle";
+import EventsPage from "./EventsPage";
 
 export const metadata: Metadata = {
   title: "Events | ICT Innnovations Africa",
@@ -12,35 +9,7 @@ export const metadata: Metadata = {
 };
 
 const Events = () => {
-  return (
-    <>
-      {/* <Breadcrumb
-        pageName="Events"
-        description="Here are some of our latest upcoming events and engagements. This would be a perfect opportunity to learn more about what we do and what we are all about."
-      /> */}
-      <section className="pb-[120px] pt-[120px]">
-        <SectionTitle
-          title="Our Latest Events"
-          paragraph="Here are some of our latest upcoming events and engagements. This would be a perfect opportunity to learn more about what we do and what we are all about."
-          center
-          buttonText="Register Interest"
-          buttonTextLink="https://forms.gle/gkxDxfg6RWreZBncA"
-        />
-        <div className="container">
-          <div className="-mx-4 flex flex-wrap justify-center">
-            {eventData.map((event) => (
-              <div
-                key={event.id}
-                className="w-full px-4 md:w-2/3 lg:w-1/2 xl:w-1/3"
-              >
-                <SingleEvent event={event} />
-              </div>
-            ))}
-          </div>
-        </div>
-      </section>
-    </>
-  );
+  return <EventsPage />;
 };
 
 export default Events;
