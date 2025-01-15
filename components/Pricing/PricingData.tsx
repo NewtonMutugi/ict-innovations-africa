713ad63d1690134795af4224602469297bb96478
NewtonMutugi <newtonmut197@gmail.com>
2025-01-15 20:51:44 +0300

    Created Pricing data

---
+++ components/Pricing/PricingData.tsx
@@ -0,0 +1,64 @@
+const pricingData = [
+  {
+    id: 1,
+    title: "Basic",
+    subtitle: "For small teams or office",
+    monthlyPrice: "$1",
+    yearlyPrice: "$10",
+    features: [
+      "Unlimited Email Accounts",
+      "10 GB Storage",
+      "100 GB Monthly Bandwidth",
+      "1 FTP Account",
+      "1 Database",
+      "Free SSL",
+    ],
+  },
+  {
+    id: 2,
+    title: "Standard",
+    subtitle: "For medium-sized businesses",
+    monthlyPrice: "$2",
+    yearlyPrice: "$25",
+    features: [
+      "Unlimited Email Accounts",
+      "25 GB Storage",
+      "200 GB Monthly Bandwidth",
+      "3 FTP Account",
+      "3 Database",
+      "Free SSL",
+    ],
+  },
+  {
+    id: 3,
+    title: "Advanced",
+    subtitle: "For larger businesses",
+    monthlyPrice: "$3",
+    yearlyPrice: "$35",
+    features: [
+      "Unlimited Email Accounts",
+      "40 GB Storage",
+      "500 GB Monthly Bandwidth",
+      "5 FTP Account",
+      "5 Database",
+      "Free SSL",
+    ],
+  },
+  {
+    id: 4,
+    title: "Premium",
+    subtitle: "For larger businesses",
+    monthlyPrice: "$4",
+    yearlyPrice: "$55",
+    features: [
+      "Unlimited Email Accounts",
+      "60 GB Storage",
+      "1 TB Monthly Bandwidth",
+      "10 FTP Account",
+      "10 Database",
+      "Free SSL",
+    ],
+  },
+];
+
+export default pricingData;
