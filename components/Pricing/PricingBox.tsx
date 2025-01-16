8428808d114d5cb59623546ef5662cb2cb33b027
NewtonMutugi <newtonmut197@gmail.com>
2025-01-16 20:30:49 +0300

    Add hosting form to pricing box with open/close functionality

--- components/Pricing/PricingBox.tsx
+++ components/Pricing/PricingBox.tsx
@@ -1,11 +1,23 @@
+import { useState } from "react";
+import HostingForm from "./HostingForm";
+
 const PricingBox = (props: {
   price: string;
   duration: string;
   packageName: string;
   subtitle: string;
   children: React.ReactNode;
+  key: number;
 }) => {
-  const { price, duration, packageName, subtitle, children } = props;
+  const { price, duration, packageName, subtitle, children, key } = props;
+  const [open, setOpen] = useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+  const handleClose = () => {
+    setOpen(false);
+  };
 
   return (
     <div className="w-full">
@@ -23,9 +35,13 @@ const PricingBox = (props: {
         </div>
         <p className="mb-7 text-base text-body-color">{subtitle}</p>
         <div className="mb-8 border-b border-body-color border-opacity-10 pb-8 dark:border-white dark:border-opacity-10">
-          <button className="flex w-full items-center justify-center rounded-sm bg-primary p-3 text-base font-semibold text-white transition duration-300 ease-in-out hover:bg-opacity-80 hover:shadow-signUp">
+          <button
+            className="flex w-full items-center justify-center rounded-sm bg-primary p-3 text-base font-semibold text-white transition duration-300 ease-in-out hover:bg-opacity-80 hover:shadow-signUp"
+            onClick={handleClickOpen}
+          >
             Purchase now
           </button>
+          <HostingForm open={open} handleClose={handleClose} id={key} />
         </div>
         <div>{children}</div>
         <div className="absolute bottom-0 right-0 z-[-1]">
