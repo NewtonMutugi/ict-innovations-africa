0b27adf5c9da0d2aca835486f9d658a529d4d4cb
NewtonMutugi <newtonmut197@gmail.com>
2025-02-24 18:33:26 +0300

    Update contact page metadata and improve newsletter subscription box text

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -4,19 +4,18 @@ import Contact from "@/components/Contact";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "ICT Innovations Africa",
+  title: "Contact | ICT Innovations Africa",
   description:
     "ICT Innovations Africa is a leading technology solutions provider dedicated to fostering innovation and digital transformation across the African continent. Our team of experts combines local insights with global best practices to develop tailored solutions that meet the unique needs of our clients.",
   // other metadata
 };
 
-
 const ContactPage = () => {
   return (
     <>
       <Breadcrumb
-        pageName="Contact Page"
-        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. In varius eros eget sapien consectetur ultrices. Ut quis dapibus libero."
+        pageName="Contact Us"
+        description="Get in touch with us today!"
       />
 
       <Contact />
